f30b928fdd2099fdb5744109ff123de4c9eb5ee7
Sandra Pérez <67371092+sanperb@users.noreply.github.com>
2020-12-20 13:30:59 +0000

    Add files via upload

---
+++ components/Firebase/firebaseConfig.js
@@ -0,0 +1,13 @@
+// Rename this file to "firebaseConfig.js" before use
+// Replace all XXXXs with real Firebase API keys
+
+export default {
+  apiKey: "AIzaSyAF6TSpisvER2VjOZBGQsC4NBoXB2Fr9xs",
+    authDomain: "maiagram-40dbb.firebaseapp.com",
+    databaseURL: "https://maiagram-40dbb.firebaseio.com",
+    projectId: "maiagram-40dbb",
+    storageBucket: "maiagram-40dbb.appspot.com",
+    messagingSenderId: "931875273828",
+    appId: "1:931875273828:web:992b8b51aa5415af86f371",
+    measurementId: "G-BZM9N5J7VW"
+};
